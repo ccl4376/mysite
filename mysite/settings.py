606139d111d7b18f0fa41cb613ff7da0e98ae103
ccl <fnt437@gmail.com>
2016-05-08 11:21:55 +0800

    not finish yet

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'haystack',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -112,3 +113,10 @@ EMAIL_HOST_USER = 'joe.lin@istayreal.com'
 EMAIL_HOST_PASSWORD = 'cclin@123'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://127.0.0.1:8983/solr/blog'
+    },
+}
